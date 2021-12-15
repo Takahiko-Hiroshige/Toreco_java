5264f56713533e0dfa4a7ecae02f153977333ea1
廣重 貴彦 <m.o.s.0310@ezweb.ne.jp>
2021-12-15 23:08:27 +0900

    strat

---
+++ src/main/java/com/example/demo/TorecoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorecoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorecoJavaApplication.class, args);
+	}
+
+}
